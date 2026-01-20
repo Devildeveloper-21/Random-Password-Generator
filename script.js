7d8670cf7de9e1f569a00f624d091d79c6706743
Harshwardhan <harshawardhandudhal@gmail.com>
2026-01-20 22:32:49 +0530

    add js logic for generating random string

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Character Declaration.
-let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let lowerChar = "abcdefghijklmnopqrstuvwxyz";
-let numbers = ["1", "2", "3", "4", "5", "6", "7", "7", "8", "9", "0"];
-let specialChar = "!@$&^#";
+let numbersArray = ["1", "2", "3", "4", "5", "6", "7", "7", "8", "9", "0"];
+let uppercaseArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerCaseArray = "abcdefghijklmnopqrstuvwxyz";
+let symbolArray = "!@$&#^";
 
 // Variable Declarations.
 const passwordLengthBar = document.getElementById("passwordLengthBar");
@@ -12,37 +12,77 @@ const upperCharSelector = document.getElementById("upperCharSelector");
 const lowerCharSelector = document.getElementById("lowerCharSelector");
 const specialCharSelector = document.getElementById("specialCharSelector");
 const generatePass = document.getElementById("generatePass");
-const addNumber = false,
-  addUpperChar = false,
-  addLowerChar = false,
-  addSpecialChar = false;
-const password = "";
+const passwordBox = document.getElementById("passwordBox");
 
+let includeNumber = false,
+  includeUpperCase = false,
+  includeLowerCase = false,
+  includeSymbol = false;
+const password = [];
+let ptr = 0;
+let randomNumber = Math.floor(Math.random());
+console.log(randomNumber);
+let len = 0,
+  count = 0;
 // Length Bar.
 passwordLengthBar.value = 0;
 passwordLengthBar.addEventListener("change", function (e) {
-  const len = Math.floor(passwordLengthBar.value / 5);
+  len = Math.floor(passwordLengthBar.value / 5);
   lengthBox.innerText = len;
+  count = len;
   // console.log(`Bg No. ${len}`);
 });
 
 //Is Checkbox checked.
 function checkCategory() {
   if (numberSelector.checked === true) {
-    addNumber = true;
-  } else if (upperChar.checked === true) {
-    addUpperChar = true;
-  } else if (lowerChar.checked === true) {
-    addLowerChar = true;
-  } else if (specialChar.checked === true) {
-    addSpecialChar = true;
+    includeNumber = true;
+  }
+  if (upperCharSelector.checked === true) {
+    includeUpperCase = true;
+  }
+  if (lowerCharSelector.checked === true) {
+    includeLowerCase = true;
+  }
+  if (specialCharSelector.checked === true) {
+    includeSymbol = true;
   }
 }
 
 //Password Generator Function.
 function GeneratePass() {
-  
+  while (len != 0) {
+    randomNumber = Math.floor(Math.random() * 4);
+
+    if (includeNumber === true && randomNumber == 0) {
+      password[ptr] = numbersArray[Math.floor(Math.random() * 10)];
+      ptr++;
+      len--;
+    }
+    if (includeUpperCase === true && randomNumber == 1) {
+      password[ptr] = uppercaseArray[Math.floor(Math.random() * 26)];
+      ptr++;
+      len--;
+    }
+    if (includeLowerCase === true && randomNumber == 2) {
+      password[ptr] = lowerCaseArray[Math.floor(Math.random() * 26)];
+      ptr++;
+      len--;
+    }
+    if (includeSymbol === true && randomNumber == 3) {
+      password[ptr] = symbolArray[Math.floor(Math.random() * 6)];
+      ptr++;
+      len--;
+    }
+  }
+
+  for (let i = 0; i < count; i++) {
+    passwordBox.innerText += password[i];
+  }
 }
 
 // Generate Button
-generatePass.addEventListener("click", function () {});
+generatePass.addEventListener("click", function () {
+  checkCategory();
+  GeneratePass();
+});
