7334d88a3e8abbf2878a0680546ed66cf31b6261
Harshwardhan <harshawardhandudhal@gmail.com>
2026-01-28 20:11:05 +0530

    edited code which form infinite loop

--- script.js
+++ script.js
@@ -13,85 +13,34 @@ const lowerCharSelector = document.getElementById("lowerCharSelector");
 const specialCharSelector = document.getElementById("specialCharSelector");
 const generatePass = document.getElementById("generatePass");
 const passwordBox = document.getElementById("passwordBox");
-
-//Setting Scheckbox Flags.
-let includeNumber = false,
-  includeUpperCase = false,
-  includeLowerCase = false,
-  includeSymbol = false;
-
-//String Storing.
-const password = [];
-let ptr = 0;
-let randomNumber = Math.floor(Math.random());
-console.log(randomNumber);
-
 let len = 0;
-// Length Bar.
+
+// Handle Length bar event
 passwordLengthBar.value = 0;
 passwordLengthBar.addEventListener("change", function (e) {
   len = Math.floor(passwordLengthBar.value / 5);
   lengthBox.innerText = len;
 });
 
-//Is Checkbox checked.
-function checkCategory() {
-
-  let CheckboxCounter = 0;
-  if (numberSelector.checked === true) {
-    includeNumber = true;
-    CheckboxCounter++;
-  } else {
-    includeNumber = false;
-    CheckboxCounter--;
-  }
-  if (upperCharSelector.checked === true) {
-    includeUpperCase = true;
-    CheckboxCounter++;
-  } else {
-    includeUpperCase = false;
-    CheckboxCounter--;
-  }
-  if (lowerCharSelector.checked === true) {
-    includeLowerCase = true;
-    CheckboxCounter++;
-  } else {
-    includeLowerCase = false;
-    CheckboxCounter--;
-  }
-  if (specialCharSelector.checked === true) {
-    includeSymbol = true;
-    CheckboxCounter++;
-  } else {
-    includeSymbol = false;
-    CheckboxCounter--;
-  }
-  if (CheckboxCounter === 0) {
-    alert("Select a Category..!");
-    console.log(CheckboxCounter);
-  }
-}
-
 //Password Generator Function.
 function GeneratePass() {
-  let tempLen = len;
+  let tempLen = len,
+    randomNumber = -1;
+  let password = [];
 
   while (tempLen != 0) {
     randomNumber = Math.floor(Math.random() * 4);
 
-    if (includeNumber === true && randomNumber == 0) {
+    if (numberSelector.checked && randomNumber == 0) {
       password.push(numbersArray[Math.floor(Math.random() * 10)]);
       tempLen--;
-    }
-    if (includeUpperCase === true && randomNumber == 1) {
+    } else if (upperCharSelector.checked && randomNumber == 1) {
       password.push(uppercaseArray[Math.floor(Math.random() * 26)]);
       tempLen--;
-    }
-    if (includeLowerCase === true && randomNumber == 2) {
+    } else if (lowerCharSelector.checked && randomNumber == 2) {
       password.push(lowerCaseArray[Math.floor(Math.random() * 26)]);
       tempLen--;
-    }
-    if (includeSymbol === true && randomNumber == 3) {
+    } else if (specialCharSelector.checked && randomNumber == 3) {
       password.push(symbolArray[Math.floor(Math.random() * 6)]);
       tempLen--;
     }
@@ -104,9 +53,17 @@ function GeneratePass() {
 
 // Generate Button
 generatePass.addEventListener("click", function () {
-  checkCategory();
-  if (len === 0) {
+  if (len == 0) {
     alert("Select password length..!");
+    return;
+  } else if (
+    !numberSelector.checked &&
+    !upperCharSelector.checked &&
+    !lowerCharSelector.checked &&
+    !specialCharSelector.checked
+  ) {
+    alert("Please select at least one category!");
+    return;
   } else {
     GeneratePass();
   }
