c11911305a56d9486b2d921c12d6e5d08c60706e
Harshwardhan <harshawardhandudhal@gmail.com>
2026-03-14 23:25:47 +0530

    added themes.css and added themes loading logic

--- Angular-Version/script.js
+++ Angular-Version/script.js
@@ -6,7 +6,7 @@ const symbolArray = "!@#$^";
 
 app.controller("passwordController", function ($scope) {
   // Function To Generate Password.
-  $scope.length=0;
+  $scope.length = 0;
   function createPassword() {
     let password = [];
     let tempLen = $scope.length;
@@ -34,10 +34,74 @@ app.controller("passwordController", function ($scope) {
   $scope.showThemes = () => {
     if (!$scope.themeVisible) {
       $scope.themeVisible = true;
-    }else{
-      $scope.themeVisible=false;
+    } else {
+      $scope.themeVisible = false;
     }
   };
+
+  $scope.copyIcon = true;
+  $scope.copyPassword = () => {
+    const passwordField = angular.element(
+      document.querySelector(".passwordField"),
+    );
+    console.log(passwordField.text());
+    $scope.copyIcon = false;
+    $scope.copiedIcon = true;
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(passwordField.text());
+    } else {
+      alert("Copy Failed");
+    }
+    setTimeout(() => {
+      $scope.$apply(() => {
+        $scope.copyIcon = true;
+        $scope.copiedIcon = false;
+      });
+    }, 2000);
+  };
+
+  $scope.themeVisible = false;
+  const themeDropdownContainer = document.querySelector(
+    ".themeDropdownContainer",
+  );
+  $scope.showThemes = function () {
+    if ($scope.themeVisible === false) {
+      $scope.themeVisible = true;
+      themeDropdownContainer.style.animation =
+        "show-animation 0.4s ease forwards";
+    } else {
+      $scope.$apply(() => {
+      themeDropdownContainer.style.animation =
+        "hide-animation 0.4 ease forwards";
+      themeDropdownContainer.addEventListener(
+        "animationend",
+        function () {
+          
+            $scope.themeVisible = false;
+          },{ once: true });
+        });
+    }
+  };
+
+  $scope.selectTheme = function (theme) {
+    themeDropdownContainer.style.animation =
+      "hide-animation 0.4s ease forwards";
+    themeDropdownContainer.addEventListener("animationend", function () {
+      $scope.themeVisible = false;
+    });
+    document.documentElement.className = "";
+    document.documentElement.classList.add(theme);
+
+    localStorage.setItem("theme", theme);
+    console.log(localStorage);
+  };
+
+  // Load theme from storage
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme) {
+    document.documentElement.classList.add(savedTheme);
+  }
 
   $scope.generatePassword = () => {
     if ($scope.length == 0) {
@@ -53,5 +117,7 @@ app.controller("passwordController", function ($scope) {
     } else {
       createPassword();
     }
+    $scope.copyIcon = true;
+    $scope.copiedIcon = false;
   };
 });
